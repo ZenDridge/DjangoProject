fa60a3ee97551d91785891afb574b7cc8b440205
ZenDridge <159112278+ZenDridge@users.noreply.github.com>
2025-01-03 10:31:59 +0800

    Update settings.py

--- ICpEPHUB/settings.py
+++ ICpEPHUB/settings.py
@@ -111,7 +111,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
